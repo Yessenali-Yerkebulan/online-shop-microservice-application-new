55f59de2af2b9bfb39c115cddf596953a6f4d99a
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-19 00:04:22 +0600

    Implement Service Discovery

--- order-service/src/main/java/com/example/orderservice/model/Order.java
+++ order-service/src/main/java/com/example/orderservice/model/Order.java
@@ -3,11 +3,12 @@ package com.example.orderservice.model;
 import java.util.List;
 
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name="t_orders")
--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class OrderService {
 	private final OrderRepository orderRepository;
 
-	private final WebClient webClient;
+	private final WebClient.Builder webClientBuilder;
 	
 	public void placeOrder(OrderRequest orderRequest) {
 		Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
 				.toList();
 
 		// Call Inventory Service, and place order if product is in stock
-		InventoryResponse[] inventoryResponsesArray = webClient.get()
-				.uri("http://localhost:8082/api/inventory",
+		InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+				.uri("http://inventory-service/api/inventory",
 						uriBuilder -> uriBuilder.queryParam ("skuCode", skuCodes).build())
 				.retrieve()
 				.bodyToMono(InventoryResponse[].class)
